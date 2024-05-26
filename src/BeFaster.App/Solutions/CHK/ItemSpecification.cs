7b016e0bad0977aeaed02f61fa6555c9f7814844
sbx_user1051 <sbx_user1051@169.254.45.249>
2024-05-26 16:24:49 +0000

    Sun May 26 16:24:49 UTC 2024

--- src/BeFaster.App/Solutions/CHK/ItemSpecification.cs
+++ src/BeFaster.App/Solutions/CHK/ItemSpecification.cs
@@ -9,9 +9,10 @@ namespace BeFaster.App.Solutions.CHK
     internal class ItemSpecification
     {
         public OfferType OfferType { get; set; }
-        public int OfferMultiple2 { get; set; }
-        public int OfferMultiple {  get; set; }
+        public Tuple<int, int> OfferMultiple2 { get; set; }
+        public Tuple<int, int> OfferMultiple {  get; set; }
         public char FreebieRecepient { get; set; }
         public int BasePrice { get; set; }  
     }
 }
+
