12a1a05965d975e9b3492b9b372e5535deeac78b
sbx_user1051 <sbx_user1051@169.254.45.249>
2024-05-26 12:22:26 +0000

    Sun May 26 12:22:26 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -8,9 +8,9 @@ namespace BeFaster.App.Solutions.CHK
         public static int ComputePrice(string skus)
         {
             int a_count = 0;
-            int a_multipules_of_3 = 0;
+            int a_multiples_of_3 = 0;
             int b_count = 0;
-            int b_multipules_2 = 0;
+            int b_multiples_2 = 0;
             int c_count = 0;
             int d_count = 0;
 
@@ -21,7 +21,7 @@ namespace BeFaster.App.Solutions.CHK
                     a_count++;
                     if (a_count % 3 == 0)
                     {
-                        a_multipules_of_3++;
+                        a_multiples_of_3++;
                     }
                 }
                 else if (c == 'B')
@@ -29,7 +29,7 @@ namespace BeFaster.App.Solutions.CHK
                     b_count++;
                     if (b_count % 2 == 0)
                     {
-                        b_multipules_2++;
+                        b_multiples_2++;
                     }
                 }
                 else if (c == 'C')
@@ -46,15 +46,16 @@ namespace BeFaster.App.Solutions.CHK
                 }
             }
 
-            int remaining_a_count = a_count - (a_multipules_of_3 * 3);
-            int a_price = (a_multipules_of_3 * 130) + remaining_a_count * 50;
+            int remaining_a_count = a_count - (a_multiples_of_3 * 3);
+            int a_price = (a_multiples_of_3 * 130) + remaining_a_count * 50;
 
-            int remaining_b_count = b_count - (b_multipules_2 * 2);
-            int b_price = (b_multipules_2 * 45) + remaining_b_count * 30;
+            int remaining_b_count = b_count - (b_multiples_2 * 2);
+            int b_price = (b_multiples_2 * 45) + remaining_b_count * 30;
 
             return (d_count * 15) + (c_count * 20) + b_price + a_price;
         }
     }
 }
+
 
 
