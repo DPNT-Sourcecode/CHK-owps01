1b9f6807a6ff8810b874b112128aed2602148558
sbx_user1051 <sbx_user1051@169.254.45.249>
2024-05-26 12:17:55 +0000

    Sun May 26 12:17:55 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -7,12 +7,6 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string skus)
         {
-            Dictionary<char, int> checkoutPrices = new Dictionary<char, int>();
-            checkoutPrices['A'] = 50;
-            checkoutPrices['B'] = 30;
-            checkoutPrices['C'] = 20;
-            checkoutPrices['D'] = 15;
-
             int a_count = 0;
             int a_multipules_of_3 = 0;
             int b_count = 0;
@@ -25,7 +19,7 @@ namespace BeFaster.App.Solutions.CHK
                 if (c == 'A')
                 {
                     a_count++;
-                    if (a_count == 3)
+                    if (a_count % 3 == 0)
                     {
                         a_multipules_of_3++;
                     }
@@ -33,11 +27,10 @@ namespace BeFaster.App.Solutions.CHK
                 if (c == 'B')
                 {
                     b_count++;
-                    if (b_count == 3)
+                    if (b_count % 2 == 0)
                     {
-                        b_multplies_of_2++;
+                        b_multipules_2++;
                     }
-
                 }
                 if (c == 'C')
                 {
@@ -49,10 +42,14 @@ namespace BeFaster.App.Solutions.CHK
                 }
             }
 
-            int a_price =
-            int total_price = 0;
+            int remaining_a_count = a_count - (a_multipules_of_3 * 3);
+            int a_price = (a_multipules_of_3 * 130) + remaining_a_count * 50;
 
+            int remaining_b_count = b_count - (b_multipules_2 * 2);
+            int b_price = (b_multipules_2 * 45) + remaining_b_count * 30;
 
+            return (d_count * 15) + (c_count * 20) + b_price + a_price;
         }
     }
 }
+
