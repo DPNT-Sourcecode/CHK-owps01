ac84d8a3d13466030de049a8e6b1e61b917ee272
sbx_user1051 <sbx_user1051@169.254.45.249>
2024-05-26 12:13:40 +0000

    Sun May 26 12:13:40 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,4 +1,5 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System.Collections.Generic;
 
 namespace BeFaster.App.Solutions.CHK
 {
@@ -6,7 +7,53 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string skus)
         {
-            throw new SolutionNotImplementedException();
+            Dictionary<char, int> checkoutPrices = new Dictionary<char, int>();
+            checkoutPrices['A'] = 50;
+            checkoutPrices['B'] = 30;
+            checkoutPrices['C'] = 20;
+            checkoutPrices['D'] = 15;
+
+            int a_count = 0;
+            int a_multipules_of_3 = 0;
+            int b_count = 0;
+            int b_multipules_2 = 0;
+            int c_count = 0;
+            int d_count = 0;
+
+            foreach (char c in skus)
+            {
+                if (c == 'A')
+                {
+                    a_count++;
+                    if (a_count == 3)
+                    {
+                        a_multipules_of_3++;
+                    }
+                }
+                if (c == 'B')
+                {
+                    b_count++;
+                    if (b_count == 3)
+                    {
+                        b_multplies_of_2++;
+                    }
+
+                }
+                if (c == 'C')
+                {
+                    c_count++;
+                }
+                if (c == 'D')
+                {
+                    d_count++;
+                }
+            }
+
+            int a_price =
+            int total_price = 0;
+
+
         }
     }
 }
+
